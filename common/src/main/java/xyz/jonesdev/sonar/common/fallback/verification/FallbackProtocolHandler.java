303dc0be216ac02d00adfe08f006fdbfb2da5376
katze225 <oleksandrsidorenko093@gmail.com>
2025-04-15 19:48:05 +0300

    Removed slot check, because of false positives

--- common/src/main/java/xyz/jonesdev/sonar/common/fallback/verification/FallbackProtocolHandler.java
+++ common/src/main/java/xyz/jonesdev/sonar/common/fallback/verification/FallbackProtocolHandler.java
@@ -58,9 +58,10 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
       // Also check if the transaction ID matches the expected ID
       final long transactionId = transaction.getTransactionId();
       checkState(transactionId == expectedTransactionId,
-        "expected T ID " + expectedTransactionId + ", but got " + transactionId);
+          "expected T ID " + expectedTransactionId + ", but got " + transactionId);
 
-      // Only perform the SetHeldItem check on Java players since the Bedrock protocol breaks this check.
+      // Only perform the SetHeldItem check on Java players since the Bedrock protocol
+      // breaks this check.
       // I don't really know why this doesn't work on Bedrock,
       // but I think it has to do with how Geyser/floodgate translates this packet:
       // https://wiki.vg/Bedrock_Protocol#Player_Hotbar
@@ -84,18 +85,22 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
       final SetHeldItemPacket heldItemPacket = (SetHeldItemPacket) packet;
 
       final int slotId = heldItemPacket.getSlot();
-      // Also check if the player sent an invalid slot which is impossible by vanilla protocol
-      checkState(slotId >= 0 && slotId <= 8, "slot out of range: " + slotId);
-      // Check if the player sent a duplicate slot packet which is impossible by vanilla protocol
-      checkState(slotId != currentClientSlotId, "duplicate slot: " + slotId);
+      // TODO: Removed slot check, because of false positives
+      // Also check if the player sent an invalid slot which is impossible by vanilla
+      // protocol
+      // checkState(slotId >= 0 && slotId <= 8, "slot out of range: " + slotId);
+      // Check if the player sent a duplicate slot packet which is impossible by
+      // vanilla protocol
+      // checkState(slotId != currentClientSlotId, "duplicate slot: " + slotId);
 
       // Only continue checking if we're actually expecting a SetHeldItem packet
       if (expectedSlotId != -1
-        // Check if the slot ID matches the expected slot ID
-        // This can false flag if a player spams these packets, which is why we don't fail for this
-        && slotId == expectedSlotId
-        // Make sure we actually want to send a transaction at this point in time
-        && !waitingSlotConfirm) {
+          // Check if the slot ID matches the expected slot ID
+          // This can false flag if a player spams these packets, which is why we don't
+          // fail for this
+          && slotId == expectedSlotId
+          // Make sure we actually want to send a transaction at this point in time
+          && !waitingSlotConfirm) {
         sendTransaction();
         waitingSlotConfirm = true;
       }
@@ -109,7 +114,7 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
         if (user.getProtocolVersion().lessThan(ProtocolVersion.MINECRAFT_1_8)) {
           // Check if the entity ID is the player itself
           checkState(animationPacket.getEntityId() == PLAYER_ENTITY_ID,
-            "invalid entity ID " + animationPacket.getEntityId());
+              "invalid entity ID " + animationPacket.getEntityId());
           // Check if the player is sending the correct animation type
           if (animationPacket.getType() == AnimationPacket.LegacyAnimationType.SWING_ARM) {
             markSuccess();
@@ -117,7 +122,7 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
           }
         } else {
           checkState(animationPacket.getHand() == AnimationPacket.MAIN_HAND,
-            "invalid hand " + animationPacket.getHand());
+              "invalid hand " + animationPacket.getHand());
           markSuccess();
           waitingSwingArm = false;
         }
@@ -126,9 +131,11 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
   }
 
   /**
-   * Uses Transaction (ping) packets to check for an immediate, legitimate response from the client
+   * Uses Transaction (ping) packets to check for an immediate, legitimate
+   * response from the client
    * <br>
-   * <a href="https://wiki.vg/Protocol#Ping_.28configuration.29">Wiki.vg - Ping (configuration)</a>
+   * <a href="https://wiki.vg/Protocol#Ping_.28configuration.29">Wiki.vg - Ping
+   * (configuration)</a>
    * <a href="https://wiki.vg/Protocol#Ping_.28play.29">Wiki.vg - Ping (play)</a>
    */
   private void sendTransaction() {
@@ -141,16 +148,22 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
   /**
    * Uses SetHeldItem packets to check for a legitimate response from the client
    * <br>
-   * <a href="https://wiki.vg/Protocol#Set_Held_Item_.28serverbound.29">Wiki.vg - SetHeldItem (play)</a>
+   * <a href="https://wiki.vg/Protocol#Set_Held_Item_.28serverbound.29">Wiki.vg -
+   * SetHeldItem (play)</a>
    */
   private void sendSetHeldItem() {
-    // Send an invalid HeldItemChange packet to the player to see if the player responds at all
+    // Send an invalid HeldItemChange packet to the player to see if the player
+    // responds at all
     user.delayedWrite(INVALID_HELD_ITEM_SLOT);
-    // Increment the player's slot by a random slot, and then modulo it by the maximum slot (8)
+    // Increment the player's slot by a random slot, and then modulo it by the
+    // maximum slot (8)
     expectedSlotId = (currentClientSlotId + 1 + RANDOM.nextInt(7)) % 8;
-    // Send two SetHeldItem packets with the same slot to check if the player responds with the correct slot.
-    // By vanilla protocol, the client does not respond to duplicate SetHeldItem packets.
-    // We can take advantage of this by sending two packets with the same content to check for a valid response.
+    // Send two SetHeldItem packets with the same slot to check if the player
+    // responds with the correct slot.
+    // By vanilla protocol, the client does not respond to duplicate SetHeldItem
+    // packets.
+    // We can take advantage of this by sending two packets with the same content to
+    // check for a valid response.
     final SetHeldItemPacket heldItemPacket = new SetHeldItemPacket(expectedSlotId);
     user.delayedWrite(heldItemPacket);
     user.delayedWrite(heldItemPacket);
@@ -158,9 +171,11 @@ public final class FallbackProtocolHandler extends FallbackVerificationHandler {
   }
 
   /**
-   * Uses EntityAnimation packets to check for the excepted Animation (SwingArm) from the client.
+   * Uses EntityAnimation packets to check for the excepted Animation (SwingArm)
+   * from the client.
    * <br>
-   * <a href="https://wiki.vg/Protocol#Entity_Animation">Wiki.vg - EntityAnimation</a>
+   * <a href="https://wiki.vg/Protocol#Entity_Animation">Wiki.vg -
+   * EntityAnimation</a>
    * <a href="https://wiki.vg/Protocol#Swing_Arm">Wiki.vg - SwingArm</a>
    */
   private void sendArmAnimation() {
